cff592aee3c7e393a044de410ef10b5c39416d56
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-02-13 22:52:34 +0100

    creating the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { SuppliersComponent } from './suppliers/suppliers.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductItemComponent } from './products/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    SuppliersComponent,
+    CategoriesComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ShoppingCartComponent,
+    NavbarComponent,
+    ProductItemComponent,
   ],
   imports: [
     BrowserModule
