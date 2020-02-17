cd90973d0e9f5092e9660ff3d5e8d088cf321fd7
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-02-17 15:49:09 +0100

    categories list

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Category} from "../shared/category.model";
 
 @Component({
   selector: 'app-categories',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+
+  categories: Category[] = [
+    new Category('Batteries'),
+    new Category('BMS'),
+    new Category('Boost'),
+    new Category('Brakes'),
+    new Category('Cars'),
+    new Category('Chassis'),
+    new Category('Drive'),
+    new Category('Engines'),
+    new Category('Gears'),
+    new Category('Suspension'),
+    new Category('Tyres'),
+    new Category('Wheels'),
+  ];
 
   constructor() { }
 
