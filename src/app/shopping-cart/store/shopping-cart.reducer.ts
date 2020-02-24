e7288e015f5bfb2b401f4a845b9bcefd3d019eb1
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-02-24 23:24:10 +0100

    Shopping Cart: storing Cart data in the session

--- src/app/shopping-cart/store/shopping-cart.reducer.ts
+++ src/app/shopping-cart/store/shopping-cart.reducer.ts
@@ -2,44 +2,51 @@ import * as ShoppingCartActions from './shopping-cart.actions';
 import {LineItem} from "../../shared/line-item.model";
 import {Product} from "../../shared/product.model";
 
+let sessionLineItems: LineItem[] = JSON.parse(sessionStorage.getItem("lineItems"));
+if(!sessionLineItems){
+  sessionLineItems = [];
+}
+
+const sampleLineItems = [ // left for testing
+  new LineItem(
+    new Product(
+      "Test Title 99",
+      "Test description",
+      "http://studio408.pl/authors/skot/018.jpg",
+      99
+    ),
+    3
+  ),
+  new LineItem(
+    new Product(
+      "Test Title 22",
+      "Test description 2",
+      "http://studio408.pl/authors/skot/018.jpg",
+      22
+    ),
+    2
+  ),
+  new LineItem(
+    new Product(
+      "Test Title 7",
+      "Test description 3",
+      "http://studio408.pl/authors/skot/018.jpg",
+      7
+    ),
+    1
+  ),
+  new LineItem(
+    new Product(
+      "Test Title 4",
+      "Test description 4",
+      "http://studio408.pl/authors/skot/018.jpg",
+      4
+    ),
+  ),
+];
+
 const initialState = {
-  lineItems: [
-    new LineItem(
-      new Product(
-        "Test Title 99",
-        "Test description",
-        "http://studio408.pl/authors/skot/018.jpg",
-        99
-      ),
-      3
-    ),
-    new LineItem(
-      new Product(
-        "Test Title 22",
-        "Test description 2",
-        "http://studio408.pl/authors/skot/018.jpg",
-        22
-      ),
-      2
-    ),
-    new LineItem(
-      new Product(
-        "Test Title 7",
-        "Test description 3",
-        "http://studio408.pl/authors/skot/018.jpg",
-        7
-      ),
-      1
-    ),
-    new LineItem(
-      new Product(
-        "Test Title 4",
-        "Test description 4",
-        "http://studio408.pl/authors/skot/018.jpg",
-        4
-      ),
-    ),
-  ],
+  lineItems: sessionLineItems,
 };
 
 export function shoppingCartReducer(
@@ -60,7 +67,7 @@ export function shoppingCartReducer(
       });
 
       updatedLineItems.push(updatedLineItem);
-
+      sessionStorage.setItem("lineItems", JSON.stringify(updatedLineItems));
       return {
         ...state,
         lineItems: [
@@ -69,6 +76,7 @@ export function shoppingCartReducer(
       };
 
     default:
+      sessionStorage.setItem("lineItems", JSON.stringify(state.lineItems));
       return state;
   }
 }
