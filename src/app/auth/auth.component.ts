7e588b2605963e0189f52be8ca6ab862db48f57b
Karol Karpeta <karol.karpeta@gmail.com>
2020-04-03 12:42:23 +0200

    login form basic shape

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {AuthService} from "./auth.service";
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+  isLoginMode = true;
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+      // ...
+    } else {
+      this.authService.signup(email, password).subscribe(resData => {
+        console.log(resData);
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit() {
+  }
+
+}
